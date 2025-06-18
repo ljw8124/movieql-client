2c7bf85bc01544475ab3fa2b8f11c1c9ec3e3d14
ljw8124 <ljw8124@gmail.com>
2025-06-18 21:41:17 +0900

    hook 을 이용하여 선언형 코드작성

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,26 +1,22 @@
-import {gql, useApolloClient} from "@apollo/client";
-import {useEffect, useState} from "react";
+import {gql, useQuery} from "@apollo/client";
+
+const ALL_MOVIES = gql`
+    query getTweets {
+        allTweets {
+            id
+        }
+    }
+`;
 
 export default function Movies() {
-    const [movies, setMovies] = useState([]);
-    const client = useApolloClient();
-
-    useEffect(() => {
-       client.query({
-           query: gql`
-               {
-                   allTweets {
-                       id
-                   }
-               }`
-       })
-       .then(result => {setMovies(result.data.allTweets)});
-    }, [client]);
-    return (
-        <div>
-            {movies.map(movie => (
-                <li key={movie.id}>{movie.id}</li>
-            ))}
-        </div>
-    );
+   const {data, loading, error} = useQuery(ALL_MOVIES);
+   if(loading) {
+       return <h1>Loading...</h1>
+   }
+   if(error) {
+       return <h1>Something went wrong.</h1>
+   }
+   return <ul>
+       {data.allTweets.map((tweet) => <li key={tweet.id}>{tweet.id}</li>)}
+   </ul>;
 }
